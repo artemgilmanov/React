3e36aac9bcb722fbcee83d283a34ec3d9e9b4ed0
Artem Gilmanov <84340745+artemgilmanov@users.noreply.github.com>
2024-03-15 10:29:30 +0100

    Add files via upload

---
+++ 08-reusable-components/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MyComponent from './components/MyComponent';
+import ParagraphComponent from './components/ParagraphComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <MyComponent />
+      <ParagraphComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ 08-reusable-components/src/components/ParagraphComponent.js
@@ -0,0 +1,21 @@
+function ParagraphComponent() {
+  return (
+    <div>
+      <p>
+        8-bit gorpcore raw denim tattooed lumbersexual meditation cray blackbird
+        spyplane subway tile forage authentic hammock kitsch. Cloud bread man
+        braid cardigan kogi poutine knausgaard bitters selvage offal keytar
+        brunch sriracha street art mukbang. Mustache authentic pop-up mumblecore
+        bushwick sustainable cardigan meh. Vexillologist pok pok truffaut
+        vibecession taiyaki cronut fanny pack cardigan beard butcher activated
+        charcoal bitters meggings post-ironic you probably haven't heard of
+        them. Celiac activated charcoal dreamcatcher, gluten-free pok pok kogi
+        heirloom mukbang. Food truck echo park godard quinoa marxism. Cloud
+        bread synth tacos, tumblr fixie blog adaptogen keffiyeh knausgaard
+        semiotics normcore messenger bag.
+      </p>
+    </div>
+  );
+}
+
+export default ParagraphComponent;
