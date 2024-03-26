c3480a24769bb934712e66c9ffaaaf80632c1b6f
Artem Gilmanov <84340745+artemgilmanov@users.noreply.github.com>
2024-03-26 08:08:10 +0100

    Add files via upload

---
+++ 14-map-array-of-objects/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Persons from './comonents/Persons';
+
+function App() {
+  return (
+    <div className="App">
+      <Persons />
+    </div>
+  );
+}
+
+export default App;
