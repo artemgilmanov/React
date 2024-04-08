5a1cf328049bce375cb71f3800b80195dcfbbf42
Artem Gilmanov <84340745+artemgilmanov@users.noreply.github.com>
2024-04-08 14:25:35 +0200

    Add files via upload

---
+++ 17-uncontrolled-input/src/components/LoginEmptyForm.js
@@ -0,0 +1,20 @@
+function LoginEmptyForm() {
+  return (
+    <>
+      <h1>Login Form:</h1>
+      <form>
+        <label>
+          Username:
+          <input type="text" name="username" />
+        </label>
+        <label>
+          Password:
+          <input type="password" name="password" />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
+}
+
+export default LoginEmptyForm;
