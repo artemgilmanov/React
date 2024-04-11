8f41fd6b3ee0b774f8940bb948f2854ded24e1ec
Artem Gilmanov <84340745+artemgilmanov@users.noreply.github.com>
2024-04-11 08:47:06 +0200

    Add files via upload

---
+++ 20-useEffect/src/App.js
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [todo, setTodo] = useState(null);
+
+  useEffect(() => {
+    console.log('Callback in useEffect called');
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response) => response.json())
+      .then((json) => setTodo(json));
+  }, []);
+
+  console.log('App Rendered');
+  console.log(todo);
+
+  return <div className="App">{todo && <h1>{todo.title}</h1>}</div>;
+}
+
+export default App;
